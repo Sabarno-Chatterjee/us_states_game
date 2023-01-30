6c5beddd0cfbd147c6a7a61c140d78ac09124678
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-30 20:37:37 +0530

    edit main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import turtle
-
+import pandas
 
 screen = turtle.Screen()
 screen.title("US States Game")
@@ -9,11 +9,21 @@ screen.addshape(image)
 turtle.shape(image)
 
 answer_text = screen.textinput(title="Guess the state", prompt="What's another state name?")
+answer_text = answer_text.title()
+
+data = pandas.read_csv("50_states.csv")
+
+x_position = data[data.state == answer_text].x
+y_position = data[data.state == answer_text].y
+x = int(x_position)
+y = int(y_position)
 
 
-
-
-
+timmy = turtle.Turtle()
+timmy.penup()
+timmy.hideturtle()
+timmy.goto(x, y)
+timmy.write(f"{answer_text}", font=("Courier", 8, "normal"))
 
 
 
