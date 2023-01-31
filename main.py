a3ac5880bf8390701c08dd28fe35cf74831bcc0f
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2023-01-31 21:23:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ screen = turtle.Screen()
 screen.title("US States Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
+turtle.shape(image)
 
 # GAME VARIABLES:
 count = 0
-turtle.shape(image)
+
 data = pandas.read_csv("50_states.csv")
 states = data.state.to_list()
 guessed_states = []
