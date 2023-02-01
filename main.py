e0e293bdde94463f42712a720306ee37ccbb2e35
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-02-01 16:08:36 +0530

    added list comprehension

--- main.py
+++ main.py
@@ -29,10 +29,8 @@ while len(guessed_states) < 50:
         timmy.goto(int(state_data.x),int(state_data.y))
         timmy.write(f"{state_data.state.item()}", font=("Courier", 8, "normal"))
 
-missing_states = []
-for state in states:
-    if state not in guessed_states:
-        missing_states.append(state)
+missing_states = [state for state in states if state not in guessed_states]
+
 
 missing_data = pandas.DataFrame(missing_states)
 missing_data.to_csv("Missing_states.csv")
